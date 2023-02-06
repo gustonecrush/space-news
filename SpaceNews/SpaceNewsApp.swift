a7bf463c5595fd6258a29ec5b987cb56c7b01bb5
gustonecrush <codeboxezz@gmail.com>
2023-02-06 22:41:42 +0700

    initialized project

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by A Farhan Agustiansyah on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
