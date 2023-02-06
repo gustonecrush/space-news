0b979b30dc8f28283524c4dd742c8af080060f8b
gustonecrush <codeboxezz@gmail.com>
2023-02-06 23:07:56 +0700

    setted up content view

--- SpaceNews/Views/ContentView.swift
+++ SpaceNews/Views/ContentView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var data = SpaceAPI()
+    @State private var opac = 0.0
 
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                NewsView()
+                    .opacity(opac)
             }
         }
         .navigationTitle("Space News")
         .environmentObject(data)
         .onAppear {
             data.getData()
+            
+            withAnimation(.easeIn(duration: 2)) {
+                opac = 1.0
+            }
         }
     }
 }
