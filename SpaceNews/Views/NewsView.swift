2932825e34243cef910af3dce6e175db31a786ce
gustonecrush <codeboxezz@gmail.com>
2023-02-06 23:09:12 +0700

    created newsview file

---
+++ SpaceNews/Views/NewsView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsView.swift
+//  SpaceNews
+//
+//  Created by A Farhan Agustiansyah on 06/02/23.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsView()
+    }
+}
