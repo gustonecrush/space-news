9156b549d8e5296519cf30294bbf9494e4d2a35b
gustonecrush <codeboxezz@gmail.com>
2023-02-06 23:17:02 +0700

    created news article

---
+++ SpaceNews/Views/NewsArticle.swift
@@ -0,0 +1,51 @@
+//
+//  NewsArticle.swift
+//  SpaceNews
+//
+//  Created by A Farhan Agustiansyah on 06/02/23.
+//
+
+import SwiftUI
+import CachedAsyncImage
+
+struct NewsArticle: View {
+    let title: String
+    let imageUrl: String
+    let siteName: String
+    let summary: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(siteName)
+                .foregroundColor(.blue)
+                .italic()
+            
+            HStack(alignment: .center) {
+                CachedAsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .easeInOut)) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    } else {
+                        // placeholder
+                    }
+                }
+            }
+            Text(title)
+                .font(.headline)
+                .padding(8)
+            
+            Text(summary)
+                .lineLimit(6)
+                .font(.body)
+                .padding(8)
+        }
+    }
+}
+
+struct NewsArticle_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsArticle(title: "Code Palace", imageUrl: "...", siteName: "CodePalace Youtube", summary: "Check out Code Palace for more cool tutorials.")
+    }
+}
